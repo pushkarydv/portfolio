d2b526526dc5c97c9e96e813e3cfd70bc299bae3
Pushkar Yadav <96358784+pushkarydv@users.noreply.github.com>
2022-12-30 16:17:31 +0530

    hero section

--- pages/_app.js
+++ pages/_app.js
@@ -4,7 +4,7 @@ import "../styles/globals.css";
 export default function App({ Component, pageProps }) {
   // TODO: ADD CUSTOM FONT
   return (
-    <main className="custom-font">
+    <main className="custom-font min-h-screen w-full bg-slate-50 text-slate-800">
       <Component {...pageProps} />
     </main>
   );
