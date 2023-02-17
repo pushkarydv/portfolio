ca8a5cf15b942449df8d29ef0847ae47f15a1507
Pushkar Yadav <96358784+pushkarydv@users.noreply.github.com>
2023-02-18 00:31:32 +0530

    Stats UI Improv

--- components/Home/Stats.js
+++ components/Home/Stats.js
@@ -9,34 +9,42 @@ export default function Stats() {
           <span className="text-4xl font-semibold">Stats</span> <br />
           &apos;&apos; Some cool numbers &apos;&apos;
         </div>
-        <div className="flex flex-row gap-4 font-semibold justify-between w-10/12 mx-auto items-center text-center my-6">
-          <div className="bg-emerald-200 px-2 w-full py-6 rounded-xl">
-            <span className="text-6xl">🚀</span>
-            <span className="text-center text-5xl text-emerald-900 block mt-6 mb-4 font-bold">
-              16+
-            </span>
-            <span className="text-3xl">Products</span>
+        <div className="flex flex-row flex-wrap font-semibold justify-between w-10/12 mx-auto items-center text-center my-6">
+          <div className="w-1/2 md:w-1/4 p-2">
+            <div className="bg-emerald-200 px-2 py-6 rounded-xl">
+              <span className="text-5xl md:text-6xl">🚀</span>
+              <span className="text-center text-4xl md:text-5xl text-emerald-900 block mt-6 mb-4 font-bold">
+                16+
+              </span>
+              <span className="text-2xl md:text-3xl">Products</span>
+            </div>
           </div>
-          <div className="bg-fuchsia-200 px-2 w-full py-6 rounded-xl">
-            <span className="text-6xl">👤</span>
-            <span className="text-center text-5xl text-fuchsia-900 block mt-6 mb-4 font-bold">
-              15K+
-            </span>
-            <span className="text-3xl"> Users</span>
+          <div className="w-1/2 md:w-1/4 p-2">
+            <div className="bg-fuchsia-200 px-2 py-6 rounded-xl">
+              <span className="text-5xl md:text-6xl">👤</span>
+              <span className="text-center text-4xl md:text-5xl text-fuchsia-900 block mt-6 mb-4 font-bold">
+                15K+
+              </span>
+              <span className="text-2xl md:text-3xl"> Users</span>
+            </div>
           </div>
-          <div className="bg-yellow-200 px-2 w-full py-6 rounded-xl">
-            <span className="text-6xl">📸</span>
-            <span className="text-center text-5xl text-yellow-900 block mt-6 mb-4 font-bold">
-              150K+
-            </span>
-            <span className="text-3xl"> Photography</span>
+          <div className="w-1/2 md:w-1/4 p-2">
+            <div className="bg-yellow-200 px-2 py-6 rounded-xl">
+              <span className="text-5xl md:text-6xl">📸</span>
+              <span className="text-center text-4xl md:text-5xl text-yellow-900 block mt-6 mb-4 font-bold">
+                150K+
+              </span>
+              <span className="text-2xl md:text-3xl"> Pexels</span>
+            </div>
           </div>
-          <div className="bg-pink-200 px-2 w-full py-6 rounded-xl">
-            <span className="text-6xl">🌍</span>
-            <span className="text-center text-5xl text-pink-900 block mt-6 mb-4 font-bold">
-              104+
-            </span>
-            <span className="text-3xl"> Countries</span>
+          <div className="w-1/2 md:w-1/4 p-2">
+            <div className="bg-pink-200 px-2 py-6 rounded-xl">
+              <span className="text-5xl md:text-6xl">🌍</span>
+              <span className="text-center text-4xl md:text-5xl text-pink-900 block mt-6 mb-4 font-bold">
+                104+
+              </span>
+              <span className="text-2xl md:text-3xl"> Countries</span>
+            </div>
           </div>
         </div>
       </div>
