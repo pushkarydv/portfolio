2db21da422f13816dc726a57b7f5d25fb2a5fcb5
Pushkar Yadav <96358784+pushkarydv@users.noreply.github.com>
2023-02-18 16:16:14 +0530

    Doodle Improv

--- components/micorcomponents/Doodle.js
+++ components/micorcomponents/Doodle.js
@@ -5,11 +5,13 @@ export default function Doodle() {
     <div className="w-44 aspect-square bg-emerald-500 absolute z-50 left-1/2 top-0 -translate-x-1/2 -translate-y-[45%] rounded-full face">
       <div className="eye w-12 aspect-square bg-white rounded-full absolute left-6 top-1/2">
         <div className="eye-parent relative w-12 aspect-square">
-          <div className="eye-view h-4 aspect-[3/5] absolute bottom-1 left-1/2 -translate-x-1/2 bg-red-400 rounded-full"></div>
+          <div className="eye-view h-4 aspect-[3/5] absolute -translate-x-1/2 bg-red-400 rounded-full"></div>
         </div>
       </div>
       <div className="eye w-12 aspect-square bg-white rounded-full absolute right-6 top-1/2">
-        <div className="eye-view h-4 aspect-[3/5] absolute bottom-1 left-1/2 -translate-x-1/2 bg-red-400 rounded-full"></div>
+        <div className="eye-parent relative w-12 aspect-square">
+          <div className="eye-view h-4 aspect-[3/5] absolute -translate-x-1/2 bg-red-400 rounded-full"></div>
+        </div>
       </div>
     </div>
   );
