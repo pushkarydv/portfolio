e53655449de65a82ea08bfccfb76eae47c0b7d7f
Pushkar Yadav <96358784+pushkarydv@users.noreply.github.com>
2023-03-17 09:24:39 +0530

    Improv: HomeStats

--- components/Home/HomeStats.js
+++ components/Home/HomeStats.js
@@ -26,40 +26,60 @@ export default function Stats() {
         </div>
         <div className="flex flex-row flex-wrap font-semibold justify-between w-full sm:w-8/12 md:w-full lg:w-10/12 mx-auto items-center text-center my-6 stats-intersection-flex">
           <div className="w-1/2 md:w-1/4 p-2 md:p-1 lg:p-4 intersection-flex">
-            <div className="bg-emerald-200 px-2 py-6 rounded-xl">
+            <a
+              target="_blank"
+              rel="noopener noreferrer"
+              href="https://www.producthunt.com/@pushkaryadav"
+              className="bg-emerald-200 px-2 py-6 rounded-xl block"
+            >
               <span className="text-5xl md:text-6xl">🚀</span>
               <span className="text-center text-4xl md:text-5xl text-emerald-900 block mt-6 mb-4 font-bold">
                 16+
               </span>
               <span className="text-2xl md:text-3xl">Products</span>
-            </div>
+            </a>
           </div>
           <div className="w-1/2 md:w-1/4 p-2 md:p-1 lg:p-4 intersection-flex">
-            <div className="bg-fuchsia-200 px-2 py-6 rounded-xl">
+            <a
+              target="_blank"
+              rel="noopener noreferrer"
+              href="https://github.com/pushkarydv"
+              className="bg-fuchsia-200 px-2 py-6 rounded-xl block"
+            >
               <span className="text-5xl md:text-6xl">👤</span>
               <span className="text-center text-4xl md:text-5xl text-fuchsia-900 block mt-6 mb-4 font-bold">
                 15K+
               </span>
               <span className="text-2xl md:text-3xl"> Users</span>
-            </div>
+            </a>
           </div>
           <div className="w-1/2 md:w-1/4 p-2 md:p-1 lg:p-4 intersection-flex">
-            <div className="bg-yellow-200 px-2 py-6 rounded-xl">
+            <a
+              target="_blank"
+              rel="noopener noreferrer"
+              href="https://www.pexels.com/@pushkaryadav"
+              className="bg-yellow-200 px-2 py-6 rounded-xl block"
+            >
               <span className="text-5xl md:text-6xl">📸</span>
               <span className="text-center text-4xl md:text-5xl text-yellow-900 block mt-6 mb-4 font-bold">
                 150K+
               </span>
               <span className="text-2xl md:text-3xl"> Pexels</span>
-            </div>
+            </a>
           </div>
           <div className="w-1/2 md:w-1/4 p-2 md:p-1 lg:p-4 intersection-flex">
-            <div className="bg-pink-200 px-2 py-6 rounded-xl">
+            <a
+              target="_blank"
+              rel="noopener noreferrer"
+              href="https://github.com/pushkarydv"
+              className="bg-pink-200 px-2 py-6 rounded-xl block"
+            >
               <span className="text-5xl md:text-6xl">🌍</span>
               <span className="text-center text-4xl md:text-5xl text-pink-900 block mt-6 mb-4 font-bold">
                 104+
               </span>
               <span className="text-2xl md:text-3xl"> Countries</span>
-            </div>
+            </a>
           </div>
         </div>
       </div>
