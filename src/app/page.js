b3243e5779c327ec323d30e4d545488dd71b48e3
Pushkar Yadav <96358784+pushkarydv@users.noreply.github.com>
2023-09-18 03:02:41 +0530

    Message Gator Production v1

--- src/app/page.js
+++ src/app/page.js
@@ -7,13 +7,13 @@ import HomeHero from "@/components/Home/HomeHero";
 import HomeProjects from "@/components/Home/HomeProjects";
 import HomeStats from "@/components/Home/HomeStats";
 
-// import { initMessageGator } from "message-gator";
+import { initMessageGator } from "message-gator";
 import { useEffect } from "react";
 
 export default function Home() {
-  // useEffect(()=>{
-  //   initMessageGator('485a5f50-1e3c-4f3b-942b-2cd7aa280f42');
-  // },[])
+  useEffect(()=>{
+    initMessageGator({userId: '7901d8b8-0b51-48fa-a9a7-d37f94e07d7f'});
+  },[])
   return (
     <main>
       <div className='bg-neutral-800 text-teal-300 p-2'>
