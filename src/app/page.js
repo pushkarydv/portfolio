88636656eee20e82571e7fe3092a40b9797d454e
Pushkar Yadav <96358784+pushkarydv@users.noreply.github.com>
2024-01-13 16:32:11 +0530

    Add web-frame-lib and update package.json

--- src/app/page.js
+++ src/app/page.js
@@ -1,28 +1,32 @@
 'use client';
+import { useEffect } from 'react';
+import { initWebFrame } from 'web-frame-lib';
 
-import HomeAbout from "@/components/Home/HomeAbout";
-import HomeExperince from "@/components/Home/HomeExperince";
-import HomeFooter from "@/components/Home/HomeFooter";
-import HomeHero from "@/components/Home/HomeHero";
-import HomeProjects from "@/components/Home/HomeProjects";
-import HomeStats from "@/components/Home/HomeStats";
-
-import { initMessageGator } from "message-gator";
-import { useEffect } from "react";
+import HomeExperince from '@/components/Home/HomeExperince';
+import HomeFooter from '@/components/Home/HomeFooter';
+import HomeHero from '@/components/Home/HomeHero';
+import HomeProjects from '@/components/Home/HomeProjects';
+import HomeStats from '@/components/Home/HomeStats';
 
 export default function Home() {
-  // useEffect(()=>{
-  //   initMessageGator({userId: '7901d8b8-0b51-48fa-a9a7-d37f94e07d7f'});
-  // },[])
+  useEffect(() => {
+    initWebFrame({
+      url: 'https://mg.writewrap.in/widget/clrbyc3ie0000j8lc9zdhosjk',
+      config: {
+        activatorText: 'Message Me',
+        css: '#wfl-frame{border: 2px solid #000; background: #fff;} @media (max-width: 450px){#wfl-frame{border:none;}}',
+      },
+    });
+  }, []);
+
   return (
     <main>
       <HomeHero />
       {/* <HomeAbout /> */}
       <HomeStats />
       <HomeProjects />
-      <HomeExperince/>
-      <HomeFooter/>
-      
+      <HomeExperince />
+      <HomeFooter />
     </main>
   );
 }
