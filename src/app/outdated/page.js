e38a00c247732286205354b8f7542569dae331a8
Pushkar Yadav <96358784+pushkarydv@users.noreply.github.com>
2024-09-30 00:22:26 +0530

    file-migrate

--- src/app/outdated/page.js
+++ src/app/outdated/page.js
@@ -1,8 +1,8 @@
-import HomeCreations from '@/components/Home/HomeCreations';
-import HomeFooter from '@/components/Home/HomeFooter';
-import HomeHero from '@/components/Home/HomeHero';
-import HomeStats from '@/components/Home/HomeStats';
-import HomeWidget from '@/components/Home/HomeWidget';
+import HomeCreations from '@/components/Home/outdated/HomeCreations';
+import HomeFooter from '@/components/Home/outdated/HomeFooter';
+import HomeHero from '@/components/Home/outdated/HomeHero';
+import HomeStats from '@/components/Home/outdated/HomeStats';
+import HomeWidget from '@/components/Home/outdated/HomeWidget';
 
 export default function Home() {
   return (
