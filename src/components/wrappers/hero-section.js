20a14a62a82e4a624480eace24b22266af161f4f
Pushkar Yadav <pushkaryadav@Pushkars-MacBook-Air.local>
2026-01-08 11:52:39 +0530

    pushkar

--- src/components/wrappers/hero-section.js
+++ src/components/wrappers/hero-section.js
@@ -1,5 +1,11 @@
 import Link from 'next/link';
 import { BorderBox } from './border-box';
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipTrigger,
+} from "@/components/ui/tooltip"
+
 
 export function HeroSection() {
   return (
@@ -11,21 +17,29 @@ export function HeroSection() {
         className='w-36 aspect-square bg-gray-200 rounded-3xl shadow-sm border-4 ml-4 mt-4'
       />
       <div className='border-l-2 border-primary mt-4 pl-4 text-3xl'>
-        Hello, I&apos;m Pushkar
+        Hello, I&apos;m <Tooltip>
+          <TooltipTrigger asChild>
+            <span className='underline decoration-dashed'>Pushkar</span>
+          </TooltipTrigger>
+          <TooltipContent>
+            <p>Push-kar &#61; push + कर (do) &rarr; push now. <br />
+              Very developer friendly name.</p>
+          </TooltipContent>
+        </Tooltip>
       </div>
       <div className='border-l-2 mt-2 pl-4 text-xl mb-4'>
         I&apos;m a Software Engineer building Cool User Interfaces and AI
         Experiments with everything in between. <br />
-         <Link
-        href='https://pushkarydv.github.io/pushkarydv/resume.pdf'
-        target='_blank'
-        rel='noopener noreferrer'
-        className='underline text-base'
-      >
-        Get Resume
-      </Link>
+        <Link
+          href='https://pushkarydv.github.io/pushkarydv/resume.pdf'
+          target='_blank'
+          rel='noopener noreferrer'
+          className='underline text-base'
+        >
+          Get Resume
+        </Link>
       </div>
-     
+
     </BorderBox>
   );
 }
